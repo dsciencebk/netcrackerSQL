871ab129ef2fa25bad15cdc9fa18b4d91cf6e9eb
Anna Volkova <dscience@bk.ru>
2016-05-04 22:27:14 +0300

    add some sqls tasks

---
+++ SQLSubqueries/4-04.sql
@@ -0,0 +1,8 @@
+Выведите все данные (*) о сотрудниках с зарплатой, равной максимальной зарплате внутри своего отдела.
+Указание: Решите задачу с использованием некоррелированного подзапроса и оператора IN.
+
+SELECT *
+FROM employees e
+WHERE salary IN (SELECT MAX(salary)
+                 FROM employees ee
+                 WHERE e.department_id=ee.department_id)
